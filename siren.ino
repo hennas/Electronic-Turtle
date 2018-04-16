d26645553eb2016c68d9e656375e0fe8b5323848
Arttuej <32383142+Arttuej@users.noreply.github.com>
2018-04-16 10:26:58 +0300

    Add files via upload

---
+++ siren.ino
@@ -0,0 +1,30 @@
+#include "pitches.h"
+int a;
+
+void setup() {
+  pinMode(3, OUTPUT);
+}
+
+void loop() { 
+  while(a < 16) {
+    //1244
+    for(int i = 440; i <= 1244; i=i+12) {
+      tone(3, i);
+      delay(3); 
+    }
+    //1243
+    for(int i = 1245; i > 440; i=i-12) {
+      tone(3, i);
+      delay(3);
+    }
+    a++;
+  }
+  a = 4;
+  while(a > 0) {
+    tone(3, 622);
+    delay(765);
+    tone(3, 440);
+    delay(765);
+    a--; 
+  }
+}
---
+++ sonar.ino
@@ -0,0 +1,48 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int trigPin = 9;
+const int echoPin = 10;
+int distanceCm, distanceInch;
+void setup() {
+  lcd.begin(16, 2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  float distance1 = sonicRead();
+  unsigned long t1 = micros();
+  delay(100);
+  float distance2 = sonicRead();
+  unsigned long t2 = micros();
+  float v = (distance2 - distance1) * 1.0 / (t2 - t1) * 10000;
+   
+  Serial.println(v);
+  delay(100);
+    
+  lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Distance: "); // Prints string "Distance" on the LCD
+  lcd.print(distance1); // Prints the distance value from the sensor
+  lcd.print(" cm");
+  delay(10);
+  lcd.setCursor(0, 1);
+  lcd.print("Speed: ");
+  lcd.print(v);
+  lcd.print(" ");
+  delay(10); 
+}
+
+float sonicRead ()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+   digitalWrite(trigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin, LOW);
+ 
+   unsigned long duration = pulseIn(echoPin, HIGH);
+   return duration * 0.034 / 2;
+}
